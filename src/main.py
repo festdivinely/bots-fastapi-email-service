82fd4f71583c638d55f2198f68a570b11805dcd6
festdivinely <festusheav@gmail.com>
2025-11-18 09:46:46 -0500

    updated python emai service 5

--- src/main.py
+++ src/main.py
@@ -34,9 +34,9 @@ class EmailRequest(BaseModel):
 
 # Email templates (import from separate files)
 # Use absolute imports
-from src.email_templates.email_verification import email_verification_template
-from src.email_templates.device_verification import device_verification_template
-from src.email_templates.password_reset import password_reset_template
+from .email_templates.email_verification import email_verification_template
+from .email_templates.device_verification import device_verification_template
+from .email_templates.password_reset import password_reset_template
 
 def get_email_template(email_type: str, data: Dict[str, Any]) -> Dict[str, str]:
     """Get the appropriate email template based on type"""
